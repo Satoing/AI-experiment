56fce488f57fafb26deeb9c5896000aa6e901737
sato <3524336191@qq.com>
2022-12-09 14:45:56 +0800

    基本完成实验3

--- 3-q-learning/agent.py
+++ 3-q-learning/agent.py
@@ -1,24 +1,48 @@
 import numpy as np
+import math
 
 
 class QLearning(object):
     def __init__(self, state_dim, action_dim, cfg):
-        self.action_dim = action_dim  # 动作的维度
-        self.lr = cfg.lr  # 学习率
-        self.gamma = cfg.gamma # 衰减系数
-        self.epsilon = 0  # 按一定概率随机选取动作
-        self.sample_count = 0
+        self.action_dim=action_dim #行动的维度
+        self.lr=cfg.lr #学习率
+        self.gamma=cfg.gamma
+        self.epsilon=0
+        self.sample_count=0
+        self.epsilon_start=cfg.epsilon_start
+        self.epsilon_end=cfg.epsilon_end
+        self.epsilon_decay=cfg.epsilon_decay
         self.Q_table = np.zeros((state_dim, action_dim))  # Q表格
 
+    # 训练过程中智能体选择动作的方法
     def choose_action(self, state):
         ####################### 智能体的决策函数，需要完成Q表格方法（需要完成）#######################
         self.sample_count += 1
-        action = np.random.choice(self.action_dim)  #随机探索选取一个动作
+        
+        # e-greedy策略
+        # epsilon是会递减的，这里选择指数递减
+        self.epsilon = self.epsilon_end+(self.epsilon_start-self.epsilon_end)\
+            *math.exp(-1.*self.sample_count/self.epsilon_decay)
+        
+        if np.random.uniform(0,1) > self.epsilon:
+            action = np.argmax(self.Q_table[state])  #选择Q(s,a)最大对应的动作
+        else:
+            action = np.random.choice(self.action_dim)  #随机选择动作
         return action
 
+    # 学习过程中更新Q表格
+    # Qlearning传入的参数为s、a、r、s，比sarsa少了下一个状态的动作
     def update(self, state, action, reward, next_state, done):
-        ############################ Q表格的更新方法（需要完成）##################################
-        pass
+        ############################## Q表格的更新方法（需要完成）##################################
+        Q_predict = self.Q_table[state][action]
+        if done: Q_target=reward
+        else: Q_target=reward+self.gamma*np.max(self.Q_table[next_state])
+        
+        self.Q_table[state][action]+=self.lr*(Q_target-Q_predict)
+        
+    def predict(self,state):
+        action=np.argmax(self.Q_table[state])
+        return action
 
     def save(self, path):
         np.save(path + "Q_table.npy", self.Q_table)
